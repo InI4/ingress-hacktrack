8bdb2fe70067a45bef4789003f22f693cd66152e
nestefan <nestefan@schaeffler.com>
2014-01-09 11:48:46 +0100

    More automatisation now.

--- pub.cmd
+++ pub.cmd
@@ -1,6 +1,11 @@
+setlocal
+pushd private
+call get.cmd
+popd
 set pf=out.html
-call fsn
+call fsn.cmd
 call git add %pf% 
 call git commit %pf% -m "New data to publish."
 call git push
 call git status
+endlocal
